f6267876f4da2ee8a804486c9a51bb3c309a44c3
Julien VANDENBUSSCHE <julien.vandenbussche@axa.fr>
2020-01-14 14:20:05 +0100

    feat(one_book): add scenario for the purchase of a book

---
+++ CSharp/test/Dojo.KataPotter.Test/EstimatePurchaseSteps.cs
@@ -0,0 +1,20 @@
+﻿namespace Dojo.KataPotter.Test
+{
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public class EstimatePurchaseSteps
+    {
+        [When("^J'achète (\\d+) copie\\(s\\) \"(.*)\"$")]
+        public void WhenJEcoleDesSorciers(int nbCopyOfBook, string title)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then("^Je doit payé (.*)€$")]
+        public void ThenJeDoitPaye(decimal expectedTotal)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
--- CSharp/test/Dojo.KataPotter.Test/EstimatePurshase.feature
+++
@@ -1,4 +0,0 @@
-﻿Feature: EstimatePurchase
-	Afin d'acheter les livres de Harry Potter
-	En tant que client
-	Je veux estimer la valeur de mon achat
