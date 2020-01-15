1ed7d57148402a0e239aa15356aa65e2ab319c93
Julien VANDENBUSSCHE <julien.vandenbussche@axa.fr>
2020-01-15 12:28:48 +0100

    feat(best_price_book): add scenario for the purchase best price books

--- TypeScript/src/bookseller.ts
+++ TypeScript/src/bookseller.ts
@@ -1,7 +1,7 @@
 import * as _ from "lodash";
 
 export class Bookseller {
-  private static readonly Discounts: number[] = [1, 0.95, 0.9, 0.8];
+  private static readonly Discounts: number[] = [1, 0.95, 0.9, 0.8, 0.75];
   public GetPrice(books: number[]): number {
     var applicableDiscount =
       Bookseller.Discounts[_.filter(books, book => book > 0).length - 1];
