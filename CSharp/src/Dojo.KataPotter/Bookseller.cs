46670abfd3e4f2e24d01cf08726656154c3f3284
Julien VANDENBUSSCHE <julien.vandenbussche@axa.fr>
2020-01-15 16:44:02 +0100

    feat(solution): add solution

--- CSharp/src/Dojo.KataPotter/Bookseller.cs
+++ CSharp/src/Dojo.KataPotter/Bookseller.cs
@@ -7,12 +7,48 @@
     {
         private const decimal UnitPrice = 8m;
 
-        private static readonly decimal[] Discounts = { 1m, 0.95m, 0.90m, 0.80m, 0.75m};
+        private static readonly decimal[] Discounts = { 1m, 0.95m, 0.90m, 0.80m, 0.75m };
 
-        public decimal GetPrice(List<int> books)
+        private delegate bool CanOptimizePrice(int volumeNumber, int remainingBook);
+
+        public decimal GetPrice(IList<int> books)
         {
-            var applicableDiscount = Discounts[books.Count(book => book > 0) - 1];
-            return books.Sum(book => book * UnitPrice) * applicableDiscount;
+            var optimizePrice = books.Count(book => book > 0) == 5 && books.Sum() >= 8;
+            return CalculatePrice(
+                (volumeNumber, remainingBook) => volumeNumber >= 4 && remainingBook > 0 && optimizePrice,
+                books);
+        }
+
+        private static decimal CalculatePrice(CanOptimizePrice canOptimizePrice, IEnumerable<int> books)
+        {
+            var filteredBooks = books.Where(item => item > 0).OrderByDescending(item => item).ToArray();
+            var numberOfBooks = HowManyAvailableBooks(canOptimizePrice, filteredBooks);
+            if (numberOfBooks == 0)
+            {
+                return 0;
+            }
+
+            var price = UnitPrice * numberOfBooks * Discounts[numberOfBooks - 1];
+            return price + CalculatePrice(canOptimizePrice, filteredBooks);
+        }
+
+        private static int HowManyAvailableBooks(CanOptimizePrice canOptimizePrice, IList<int> books)
+        {
+            int numberOfBooks = 0;
+            for (int volumeNumber = 0; volumeNumber < books.Count(); volumeNumber++)
+            {
+                if (canOptimizePrice(volumeNumber, books[volumeNumber]))
+                {
+                    continue;
+                }
+
+                if (books[volumeNumber]-- > 0)
+                {
+                    numberOfBooks++;
+                }
+            }
+
+            return numberOfBooks;
         }
     }
 }
--- TypeScript/src/bookseller.ts
+++ TypeScript/src/bookseller.ts
@@ -1,10 +1,67 @@
 import * as _ from "lodash";
 
+interface CanOptimizePrice {
+  (volumeNumber: number, remainingBook: number): boolean;
+}
+
 export class Bookseller {
+  private static readonly UnitPrice: number = 8;
   private static readonly Discounts: number[] = [1, 0.95, 0.9, 0.8, 0.75];
+
   public GetPrice(books: number[]): number {
-    var applicableDiscount =
-      Bookseller.Discounts[_.filter(books, book => book > 0).length - 1];
-    return _.sumBy(books, book => book * 8) * applicableDiscount;
+    let parsedBooks = _.map(books, (book: string) => _.parseInt(book));
+    let optimizePrice: boolean =
+      _.filter(parsedBooks, (book: number) => book > 0).length == 5 &&
+      _.sum(parsedBooks) >= 8;
+    return Bookseller.CalculatePrice(
+      (volumeNumber, remainingBook) =>
+        volumeNumber >= 4 && remainingBook > 0 && optimizePrice,
+      books
+    );
+  }
+
+  private static CalculatePrice(
+    canOptimizePrice: CanOptimizePrice,
+    books: number[]
+  ): number {
+    let filteredBooks: number[] = _.chain(books)
+      .filter(book => book > 0)
+      .sortBy("asc")
+      .value();
+    var numberOfBooks = Bookseller.HowManyAvailableBooks(
+      canOptimizePrice,
+      filteredBooks
+    );
+    if (numberOfBooks == 0) {
+      return 0;
+    }
+
+    var price =
+      Bookseller.UnitPrice *
+      numberOfBooks *
+      Bookseller.Discounts[numberOfBooks - 1];
+    return price + Bookseller.CalculatePrice(canOptimizePrice, filteredBooks);
+  }
+
+  private static HowManyAvailableBooks(
+    canOptimizePrice: CanOptimizePrice,
+    books: number[]
+  ): number {
+    let numberOfBooks: number = 0;
+    for (
+      let volumeNumber: number = 0;
+      volumeNumber < books.length;
+      volumeNumber++
+    ) {
+      if (canOptimizePrice(volumeNumber, books[volumeNumber])) {
+        continue;
+      }
+
+      if (books[volumeNumber]-- > 0) {
+        numberOfBooks++;
+      }
+    }
+
+    return numberOfBooks;
   }
 }
